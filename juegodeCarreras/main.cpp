cc66081a41180e97a5408de074fdd66f94b80842
norbertmontane <norbert.montane@estudiant.enti.cat>
2025-12-01 09:56:14 +0100

    CAMBIO 4

--- juegodeCarreras/main.cpp
+++ juegodeCarreras/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #define CASILLAS_TOTALES 50
 
+enum  PlayerTurn
+{
+	PLAYER_1,
+	PLAYER_2,
+};
+
+
 int main() {
 
-char tablero [CASILLAS_TOTALES]
+	char tablero[CASILLAS_TOTALES];
 
+	bool GameOver = false;
+	PlayerTurn playerTurn = PLAYER_1;
+
+	while (!GameOver)
+	{
+		playerTurn = playerTurn == PLAYER_1 ? PLAYER_2 : PLAYER_1;
+
+	}
+
+	std::cout << "Hello World!";
 	return 0;
 }
